0ca4a94f1b2e91afa23abb44bb728920526b7d19
mayankbhoriaDecision <80088527+mayankbhoriaDecision@users.noreply.github.com>
2022-03-31 11:27:34 +0530

    Update maphash test.java

--- maphash test.java
+++ maphash test.java
@@ -1,4 +1,4 @@
-import java.util.*;
+ import java.util.*;
 
 public class Main {
 
@@ -14,11 +14,27 @@ public class Main {
 
 		int N=in.nextInt();
 
+
+            List<Address> AddList = new ArrayList<>();
+
+
+            in.nextLine();
+
+
 		for(int i=0;i<N;i++)
 		{
 
-				System.out.println("Enter the address "+i+" detail");
-				String val=in.nextLine();
+                  int m=i+1;
+
+	      System.out.println("Enter the address "+m+" detail");
+
+
+
+
+		String val=in.nextLine();
+
+
+
             String[] res = val.split("\\,");
 
                String addressLine1=res[0];
@@ -31,22 +47,28 @@ public class Main {
 
              String pincode=res[4];
 
-             boolean  value = MegaMap.get(city);
+
+
+
+             List<Address>  value = MegaMap.get(city);
+
+
+
 
              Address AddrObj= new Address(  addressLine1,addressLine2, city,state, Integer.valueOf( pincode ) );
 
              if(value==null)
              {
-                  List<Address> AddList = new ArrayList<>();
+                  
                   AddList.add(AddrObj );
                    MegaMap.put( city,AddList  );
              }
              else
 
              {
-                  List<Address> AddList = new ArrayList<>();
-                     AddList = map.get(city);
-                    AddList.add(AddrObj );
+                   
+                   AddList = MegaMap.get(city);
+                   AddList.add(AddrObj );
                    MegaMap.put( city,AddList  );
 
              }
@@ -57,12 +79,13 @@ public class Main {
 		String cityVal=in.nextLine();
 
 
-      Foo value = MegaMap.get(cityVal);
+       List<Address> AddVals = MegaMap.get(cityVal);
 
-      if (value != null)
+      if (AddVals != null)
        {
 
-             List<Address> AddVals = MegaMap[ cityVal];
+             
+ 
 
 
             System.out.printf( "%-15s %-15s %-15s %-15s %s\n",  "Line 1" , "Line 2"  , "City" ,"State" , "Pincode"  );
@@ -70,11 +93,20 @@ public class Main {
              for (int i = 0; i < AddVals.size(); i++) 
              
              {
+                     String[] res= AddVals.get(i).getAddValue();
+
+                   if(  res[2].equals(cityVal)        )
+                   {
 
 
-                        String[] res= AddVals.get(i).getAddValue();
+  
+                         System.out.printf( "%-15s %-15s %-15s %-15s %s\n",  res[0]  ,res[1]   , res[2], res[3] ,  res[4]  );                            
 
-                         System.out.printf( "%-15s %-15s %-15s %-15s %s\n",  res[0]  ,res[1]   , res[2], res[3] ,  res[4]  );
+                   }
+
+
+                      
+
 
               }
          
@@ -87,6 +119,6 @@ public class Main {
 
 
 	
-	
+      }
 
 }
